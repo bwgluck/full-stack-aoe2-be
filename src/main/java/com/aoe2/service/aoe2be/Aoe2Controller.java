a736a35594ce1053a71f7658892171cc231ad68c
Bryan-Gluck <bryan.gluck@ibm.com>
2021-07-23 23:55:56 -0400

    Added support for cross origin.

--- src/main/java/com/aoe2/service/aoe2be/Aoe2Controller.java
+++ src/main/java/com/aoe2/service/aoe2be/Aoe2Controller.java
@@ -1,10 +1,12 @@
 package com.aoe2.service.aoe2be;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins="http://localhost:4200")
 public class Aoe2Controller {
     @GetMapping(path="/hello-world")
     public String helloWorld() {
